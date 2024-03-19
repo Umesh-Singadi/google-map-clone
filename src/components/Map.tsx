063dd28a782c1690a777a6371b6b3f6777de90fa
Umesh Singadi <umeshsingadi007@gmail.com>
2024-03-19 22:00:41 +0530

    add map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,33 @@
 import type { Place } from "../api/Place";
+import "leaflet/dist/leaflet.css";
+import { Map as LeafletMap } from "leaflet";
+import { useEffect, useRef } from "react";
+import { Marker, MapContainer, TileLayer } from "react-leaflet";
+
 interface MapProps {
   place: Place | null;
 }
+
 function Map({ place }: MapProps) {
-  return <div>Map</div>;
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[12.971599, 77.594566]}
+      zoom={12}
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+      {place && <Marker position={[place.latitude, place.longitude]}></Marker>}
+    </MapContainer>
+  );
 }
 
 export default Map;
